fa755696e86597e0835fb834be227c372a062743
octo647 <helder.couto5@@gmail.com>
2026-04-02 14:05:20 -0300

    Teste: adiciona título de debug no formulário de senha

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -15,15 +15,18 @@
     </x-slot>
 
     <div class="py-0">
+    
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
             <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
                 <div class="max-w-xl">
                     @include('profile.partials.update-profile-information-form')
+                  
                 </div>
             </div>
 
             <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
                 <div class="max-w-xl">
+                    <h2 class="text-xl font-bold mb-4">TESTE: Seção de Senha</h2>
                     @include('profile.partials.update-password-form')
                 </div>
             </div>
